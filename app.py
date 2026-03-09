7e7dd63ddcfacade9c15b561ab7d2cdf60306d5f
RAKESH-G2210 <rg2021@srmist.edu.in>
2026-03-09 12:46:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ CORS(app)
 
 @app.route("/")
 def home():
-return jsonify({
+    return jsonify({
 "message": "Legal AI Backend Running"
 })
 
@@ -283,4 +283,5 @@ port = int(os.environ.get("PORT", 5000))
 
 app.run(host="0.0.0.0", port=port)
 ```
+
 
